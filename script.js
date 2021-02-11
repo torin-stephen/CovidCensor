81cda8791788ee733cfaea66e09d7f10ee3f3d7f
Torin Stephen <tkmsmc@gmail.com>
2021-02-11 14:56:16 +0000

    update

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function replaceText(element) {
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText)
   } else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/coronavirus | covid/gi)) {
+    if (element.textContent.match(/covid/gi)) {
       element.parentElement.style.color = 'black'
       element.parentElement.style.backgroundColor = 'black'
       
     }
-    element.textContent = element.textContent.replace(/coronavirus | covid/gi,
+    element.textContent = element.textContent.replace(/covid/gi,
         '▇▇▇')
   }
 }
